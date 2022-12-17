d8192bc8ee2a517d2ff9323cd7d36cca5e795a96
gaopeng0108 <gaopeng19960108@gmail.com>
2022-12-17 22:21:49 +0800

    :sparkles: fiber tree

--- my-react-v16.8/index.js
+++ my-react-v16.8/index.js
@@ -27,17 +27,26 @@ function createElement(type, config, ...children) {
   }
 }
 
-function render(element, container) {
-  const dom = element.type === TEXT_ELEMENT ? document.createTextNode('') : document.createElement(element.type);
+function createDom(fiber) {
+  const dom = fiber.type === TEXT_ELEMENT ? document.createTextNode('') : document.createElement(fiber.type);
 
   const isProperty = (key) => key !== 'children';
-  Object.keys(element.props).filter(isProperty).forEach((key) => {
-    dom[key] = element.props[key];
-  });
+  Object.keys(fiber.props)
+    .filter(isProperty)
+    .forEach((key) => {
+      dom[key] = fiber.props[key];
+    });
+  
+  return dom;
+}
 
-  element.props.children.forEach((child) => render(child, dom));
-
-  container.appendChild(dom);
+function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
 }
 
 let nextUnitOfWork = null;
@@ -56,8 +65,52 @@ function workLoop(deadline) {
 
 requestIdleCallback(workLoop);
 
-function performUnitOfWork(nextUnitOfWork) {
-  // TODO
+function performUnitOfWork(fiber) {
+  console.log('fiber', fiber);
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+
+  while (index < elements.length) {
+    const element = elements[index]
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
+
+  if (fiber.child) {
+    return fiber.child;
+  }
+
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+
+    nextFiber = nextFiber.parent;
+  }
 }
 
 const MyReact = {
